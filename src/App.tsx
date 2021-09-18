07e43c7bbfe9a36d49a8bd95f14ede60810ce3e6
Vitor Gonçalves <vitorfgoncalves3@gmail.com>
2021-09-18 13:37:23 -0300

    starting structuring the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
-
-const App : React.FC = () => <View></View>
+import {View, StatusBar} from 'react-native';
+import Routes from './routes'
+import { NavigationContainer } from '@react-navigation/native'
+const App : React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle='light-content' backgroundColor='#312e28'/>
+    <View style={{flex:1, backgroundColor: '#312e28'}}><Routes /></View>
+  </NavigationContainer>
+)
 
 export default App;
